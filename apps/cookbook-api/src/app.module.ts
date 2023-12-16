8e4d823d19aee6de5d740c0e67c40efdb63b3110
Jonathan FLAMME <joflamme@hotmail.com>
2023-12-16 20:30:48 +0100

    [API] Init module entity

--- apps/cookbook-api/src/app.module.ts
+++ apps/cookbook-api/src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app/app.service';
+import { RecipesModule } from './recipes/recipes.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
+import { StepsModule } from './steps/steps.module';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RecipesModule,
+    IngredientsModule,
+    StepsModule,
+    CategoriesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
