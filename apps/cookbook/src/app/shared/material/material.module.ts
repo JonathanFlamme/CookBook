765a5a618fa52f8a879c112c2cb4c37510544d65
Jonathan FLAMME <joflamme@hotmail.com>
2023-12-23 15:17:23 +0100

    [cookbook] Import Mat module

--- apps/cookbook/src/app/shared/material/material.module.ts
+++ apps/cookbook/src/app/shared/material/material.module.ts
@@ -1,13 +1,37 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports: [MatSidenavModule,MatToolbarModule],
-    exports: [MatSidenavModule,MatToolbarModule],
-    declarations: [],
-    providers: [],
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
+  declarations: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'fill' },
+    },
+  ],
 })
-
-export class MaterialModule{}
+export class MaterialModule {}
--- apps/cookbook/src/app/shared/shared.module.ts
+++ apps/cookbook/src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from "@angular/core";
-import { MaterialModule } from "./material/material.module";
-import { FontAwesomeLibraryModule } from "./material/font-awesome.module";
-
-
-
+import { NgModule } from '@angular/core';
+import { MaterialModule } from './material/material.module';
+import { FontAwesomeLibraryModule } from './material/font-awesome.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    imports: [MaterialModule,FontAwesomeLibraryModule],
-    exports: [MaterialModule,FontAwesomeLibraryModule],
-    declarations: [],
-    providers: [],
+  imports: [MaterialModule, FontAwesomeLibraryModule, CommonModule],
+  exports: [MaterialModule, FontAwesomeLibraryModule, CommonModule],
+  declarations: [],
+  providers: [],
 })
-
-export class SharedModule{}
+export class SharedModule {}
