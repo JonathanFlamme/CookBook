7cac45add1513f9a8d01b75692a00f8efc1f2978
Jonathan FLAMME <joflamme@hotmail.com>
2024-01-21 19:42:05 +0100

    [cookbook] unit list added to ingredientModel

--- libs/models/src/lib/ingredient.model.ts
+++ libs/models/src/lib/ingredient.model.ts
@@ -3,6 +3,17 @@ export interface IngredientModel {
   recipeId: string;
   name: string;
   quantity: string;
+  unit: UnitList;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export enum UnitList {
+  Gram = 'gram',
+  Kilogram = 'kiloGram',
+  Liter = 'liter',
+  Centiliter = 'centiliter',
+  Milliliter = 'milliliter',
+  tablespoon = 'tablespoon',
+  teaspoon = 'teaspoon',
 }
