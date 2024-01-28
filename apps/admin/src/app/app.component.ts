4b37f09ebf8457c1607de82c2596258eb0e1bd97
Jonathan FLAMME <joflamme@hotmail.com>
2024-01-28 14:42:03 +0100

    [cookbook] add autoLogin methode

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'admin';
+
+  constructor(private readonly authService: AuthService) {}
+
+  public ngOnInit(): void {
+    this.authService.autoLogin();
+  }
 }
