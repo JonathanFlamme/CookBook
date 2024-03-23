3aa9d6542edc892c146fc8ef7e0a95513eb50946
joflamme <joflamme@gmail.com>
2024-03-23 17:38:15 +0100

    Fix message when loggin failed

--- apps/cookbook-api/src/auth/local.strategy.ts
+++ apps/cookbook-api/src/auth/local.strategy.ts
@@ -1,6 +1,6 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserEntity } from '../users/user.entity';
 
@@ -13,7 +13,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<UserEntity> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException('invalid identifier');
+      throw new Error('invalid identifier');
     }
     return user;
   }
