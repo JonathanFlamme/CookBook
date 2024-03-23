7306b20cc20f1ca8300e722397753c184990a402
joflamme <joflamme@gmail.com>
2024-03-23 19:46:23 +0100

    remove userId temporary

--- libs/models/src/lib/user.model.ts
+++ libs/models/src/lib/user.model.ts
@@ -23,10 +23,6 @@ export interface UserQuotas {
   recipePerMonth: number;
 }
 
-export enum UserIdTemporaly {
-  UserId = '3adf32aa-ae4b-4507-9b97-b1caacf15378',
-}
-
 export interface UserRequest {
   userId: string;
   role: string;
