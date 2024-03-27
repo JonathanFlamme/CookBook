70c522a243d9b5ddc51f65b40d380409b693e77a
joflamme <joflamme@gmail.com>
2024-03-27 14:38:46 +0100

    [API] Refato migration to ONE Init

--- apps/cookbook-api/migrations/1711546314147-Init.ts
+++ apps/cookbook-api/migrations/1711546314147-Init.ts
@@ -1,13 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1705864604983 implements MigrationInterface {
-    name = 'Init1705864604983'
+export class Init1711546314147 implements MigrationInterface {
+    name = 'Init1711546314147'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "step" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "recipeId" uuid NOT NULL, "description" text NOT NULL, "sort" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_70d386ace569c3d265e05db0cc7" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."user_role_enum" AS ENUM('admin', 'user')`);
-        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "role" "public"."user_role_enum" NOT NULL DEFAULT 'user', "givenName" text NOT NULL, "familyName" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "recipe" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, "title" text NOT NULL, "duration" text NOT NULL, "categories" text array NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e365a2fedf57238d970e07825ca" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "role" "public"."user_role_enum" NOT NULL DEFAULT 'user', "givenName" text NOT NULL, "familyName" text NOT NULL, "quotas" jsonb NOT NULL DEFAULT '{"recipePerMonth":1}', "email" text NOT NULL, "emailToken" json, "passwordToken" json, "password" text NOT NULL, "verifiedAt" TIMESTAMP, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "recipe" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT NULL, "imageUrl" text, "title" text NOT NULL, "duration" text NOT NULL, "categories" text array NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e365a2fedf57238d970e07825ca" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."ingredient_unit_enum" AS ENUM('gram', 'kiloGram', 'liter', 'centiliter', 'milliliter', 'tablespoon', 'teaspoon')`);
         await queryRunner.query(`CREATE TABLE "ingredient" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "recipeId" uuid NOT NULL, "name" text NOT NULL, "quantity" text NOT NULL, "unit" "public"."ingredient_unit_enum", "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_6f1e945604a0b59f56a57570e98" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "step" ADD CONSTRAINT "FK_e50600a62b8ece3b996d58331f4" FOREIGN KEY ("recipeId") REFERENCES "recipe"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
--- apps/cookbook-api/orm/migrations.ts
+++ apps/cookbook-api/orm/migrations.ts
@@ -1,15 +1,3 @@
-import { Init1705864604983 } from '../migrations/1705864604983-Init';
-import { QuotaRecipeByMonth1710000866028 } from '../migrations/1710000866028-QuotaRecipeByMonth';
-import { ImageUrl1710504658329 } from '../migrations/1710504658329-ImageUrl';
-import { EmailToken1711198186016 } from '../migrations/1711198186016-EmailToken';
-import { PasswordToken1711214559792 } from '../migrations/1711214559792-PasswordToken';
-import { UniqueEmail1711364933435 } from '../migrations/1711364933435-UniqueEmail';
+import { Init1711546314147 } from '../migrations/1711546314147-Init';
 
-export const migrations = [
-  Init1705864604983,
-  QuotaRecipeByMonth1710000866028,
-  ImageUrl1710504658329,
-  EmailToken1711198186016,
-  PasswordToken1711214559792,
-  UniqueEmail1711364933435,
-];
+export const migrations = [Init1711546314147];
